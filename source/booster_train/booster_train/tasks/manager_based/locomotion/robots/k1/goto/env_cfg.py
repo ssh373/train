63fc9e642838d91433d3e7ceb0cd5dc9728f57c7
suhyeon <sinsu321@gmail.com>
2026-07-29 16:22:59 +0900

    baseline

--- source/booster_train/booster_train/tasks/manager_based/locomotion/robots/k1/goto/env_cfg.py
+++ source/booster_train/booster_train/tasks/manager_based/locomotion/robots/k1/goto/env_cfg.py
@@ -249,7 +249,7 @@ class ObservationsCfg:
 
 @configclass
 class RewardsCfg:
-    constellation = RewTerm(func=mdp.constellation_reward, weight=1.0, params={
+    constellation = RewTerm(func=mdp.constellation_reward, weight=3.0, params={
         "command_name": "pose_goal", "radius": 1.0})
     success = RewTerm(func=mdp.goal_success, weight=2.0, params={
         "command_name": "pose_goal", "sensor_cfg": SceneEntityCfg("contact_forces", body_names=FEET)})
