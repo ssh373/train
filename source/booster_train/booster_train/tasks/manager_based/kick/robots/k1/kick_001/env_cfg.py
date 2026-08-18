c479d492c847fe40909d3d5634c6b54c0e9570e3
suhyeon <sinsu321@gmail.com>
2026-08-19 00:23:22 +0900

    edited by sol

--- source/booster_train/booster_train/tasks/manager_based/kick/robots/k1/kick_001/env_cfg.py
+++ source/booster_train/booster_train/tasks/manager_based/kick/robots/k1/kick_001/env_cfg.py
@@ -181,7 +181,7 @@ class RewardsCfg:
 
     ball_velocity_target = RewTerm(
         func=mdp.ball_velocity_to_target,
-        weight=12.0,
+        weight=18.0,
         params={"target_xy": (4.0, 0.0), "decay_distance": 4.0, "max_reward": 10.0},
     )
     ball_target_accuracy = RewTerm(
@@ -271,7 +271,7 @@ class RewardsCfg:
     )
     ball_acceleration = RewTerm(
         func=mdp.ball_acceleration_to_target,
-        weight=0.5,
+        weight=1.0,
         params={"target_xy": (4.0, 0.0), "scale": 80.0},
     )
     waiting = RewTerm(func=mdp.waiting_penalty, weight=-12.0)
